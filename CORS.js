fdf22e7ea08ca022fe1b45b10f6d47b4655a24ea
yusalz <84872458+yusalz@users.noreply.github.com>
2021-12-21 12:51:55 +0500

    Create CORS.js

---
+++ CORS.js
@@ -0,0 +1,6 @@
+export default {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,DELETE,PUT,OPTIONS",
+    "Access-Control-Expose-Headers": "X-Resp,Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Expose-Headers",
+    "Access-Control-Allow-Headers": "X-Resp,Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Expose-Headers"
+}
